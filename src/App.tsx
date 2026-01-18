ef5fce8aa15218cdf871d8dd02b3255561e56a3f
Avash Kattel <avashkattel2827@gmail.com>
2026-01-18 17:24:40 -0500

    New dash

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div className="w-screen h-screen overflow-hidden bg-background text-foreground animate-in fade-in duration-500">
         <Routes>
           <Route path="/" element={<Home />} />
